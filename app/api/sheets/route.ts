4e58e9a6d30d390db1e45ed74ece4b9dafbd5dd4
sgoulden-motorway <sgoulden@motorway.co.uk>
2023-09-19 13:42:25 +0100

    cache revalidate

--- app/api/sheets/route.ts
+++ app/api/sheets/route.ts
@@ -1,10 +1,21 @@
 import { getGoogleSheetsData } from "@/utilities/server/sheets";
-
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 export const fetchCache = "force-no-store";
 
 const range = `${process.env.NEXT_PUBLIC_SHEET_NAME}!${process.env.NEXT_PUBLIC_SHEET_RANGE}`;
 
-export async function GET(req: Request) {
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path");
+
+  if (!path) {
+    return NextResponse.json(
+      { message: "Missing path param" },
+      { status: 400 }
+    );
+  }
+
   const sheet = await getGoogleSheetsData(range);
+  revalidatePath(path);
   return new Response(JSON.stringify(sheet), { status: 200 });
 }
