7b3b318acda949bd78d0b6c3d15dd0bc321aa3bf
Sam Goulden <samgoulden28@gmail.com>
2023-11-07 16:39:05 +0000

    base64 for windows PK

--- utilities/server/sheets.ts
+++ utilities/server/sheets.ts
@@ -1,18 +1,20 @@
 import { google } from "googleapis";
 import { v4 } from "uuid";
-
-const auth = google.auth.getClient({
+// should be GSHEETS_PRIVATE_KEY if it exists or GSHEETS_PRIVATE_KEY_BASE64 decoded if it exists
+const private_key = process.env.GSHEETS_PRIVATE_KEY || process.env.GSHEETS_PRIVATE_KEY_BASE64 ? Buffer.from(process.env.GSHEETS_PRIVATE_KEY_BASE64 || "", "base64").toString("ascii") : "";
+const options = {
   projectId: "quick-line-389216",
   credentials: {
     type: "service_account",
     token_url: `https://oauth2.googleapis.com/token?salt=${v4()}`, // cache buster
     universe_domain: "googleapis.com",
-    private_key: process.env.GSHEETS_PRIVATE_KEY || "",
+    private_key,
     client_email: process.env.GSHEETS_CLIENT_EMAIL || "",
     client_id: process.env.GSHEETS_CLIENT_ID || "",
   },
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+};
+const auth = google.auth.getClient(options);
 
 export async function getGoogleSheetsData(range: string) {
   const sheets = google.sheets({ version: "v4", auth: await auth });
